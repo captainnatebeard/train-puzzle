3f47884344ef24c201d97e79af1239cc83adf413
captainnatebeard <nathanjohnson@rocketmail.com>
2023-01-26 09:21:12 -0800

    added text to buttons, formatted line numbers in code box

--- main.py
+++ main.py
@@ -13,24 +13,22 @@ stationImage = pygame.image.load("station-sm.png")
 stationLoc = (820, 60)
 trainY = 120
 trackLoc = (-15, 100)
-input_rect = pygame.Rect(600, 350, 400, 500)
+input_rect = pygame.Rect(650, 350, 400, 500)
 run_rect = pygame.Rect(300, 350, 200, 200)
+run_text_rect = pygame.Rect(380, 430, 50, 50)
 directions_rect = pygame.Rect(1200, 500, 400, 400)
 clear_rect = pygame.Rect(300, 650, 200, 200)
-color_active = pygame.Color('lightskyblue2')
-color_passive = pygame.Color('lightskyblue3')
-color = color_passive
+clear_text_rect = pygame.Rect(370, 730, 100, 100)
 base_font = pygame.font.Font(None, 32)
 train1_start = 650
 train2_start = 1000
 description = "the two trains on the track are running the same codebase, try to get them to collide in as few lines \
 as possible using only these 4 commands: MVL (move left), MVR (move right), STS (skip instruction if at the station), \
-and JMP (jump to another line in the code).  The box in the middle is for your code.  The green box runs your code.  \
-The red box resets and clears                                                                   Good luck!"
+and JMP (jump to another line in the code, ex: 'JMP 3').  The box in the middle is for your code.  The green box runs \
+your code.  The red box resets and clears                                                   Good luck!"
 
 
 def main():
-    pygame.init()
     instruction_set = []
     running = True
     active = False
@@ -41,14 +39,16 @@ def main():
     line = 0
     pc1 = 0
     pc2 = 0
+    color_active = pygame.Color('lightskyblue2')
+    color_passive = pygame.Color('lightskyblue3')
     instruction_set.append('')
     trains_running = False
     # Game Loop
     while running:
         for event in pygame.event.get():
-            if event.type == 256: #(pygame.QUIT)
+            if event.type == 256:  # (pygame.QUIT)
                 running = False
-            if event.type == 1026: #mouse click up
+            if event.type == 1026:  # mouse button release
                 if input_rect.collidepoint(event.pos):
                     active = True
                 else:
@@ -81,7 +81,7 @@ def main():
         if trains_running:
             train1_x, at_station1, pc1 = run_next_instruction(instruction_set, train1_x, at_station1, pc1)
             train2_x, at_station2, pc2 = run_next_instruction(instruction_set, train2_x, at_station2, pc2)
-        if train1_x > train2_x - 120 or pc1 >= len(instruction_set) or pc2 >= len(instruction_set):
+        if train1_x > train2_x - 115 or pc1 >= len(instruction_set) or pc2 >= len(instruction_set):
             time.sleep(5)
             trains_running = False
             train1_x = train1_start
@@ -97,13 +97,16 @@ def main():
         pygame.draw.rect(screen, pygame.Color('red'), clear_rect)
         pygame.draw.rect(screen, pygame.Color('green'), run_rect)
         for i in range(len(instruction_set)):
-            text_surface = base_font.render(str(i) + ': ' + instruction_set[i], True, (255, 255, 255))
+            text_surface = base_font.render(str('{num:02d}'.format(num=i)) + ': ' +
+                                            instruction_set[i], True, (255, 255, 255))
             screen.blit(text_surface, (input_rect.x + 5 + (120 * int(i / 22)), input_rect.y + 5 + ((i % 22) * 22)))
         screen.blit(trackImage, trackLoc)
         screen.blit(stationImage, stationLoc)
         draw_train(train1_x)
         draw_train(train2_x)
-        draw_text(screen, description, (255,255,255), directions_rect, base_font)
+        draw_text(screen, description, (255, 255, 255), directions_rect, base_font)
+        draw_text(screen, "RUN", (255, 255, 255), run_text_rect, base_font)
+        draw_text(screen, "CLEAR", (255, 255, 255), clear_text_rect, base_font)
         pygame.display.flip()
         clock.tick(30)
 
@@ -153,6 +156,7 @@ def draw_text(surface, text, color, rect, font, aa=False, bkg=None):
         y += font_height + line_spacing
         text = text[i:]
     return text
+
 
 if __name__ == "__main__":
     main()
